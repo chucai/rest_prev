371fa6ca88ce00e9142e413ff7d046fe3be53c26
chucai <hexudong08@gmail.com>
2012-09-09 16:53:16 +0800

    修改了json数据的显示

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,4 +2,6 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require_tree .
+var json = JSON.stringify(JSON.parse($(".nowrap").html()), null, 2);
+$(".nowrap").html(json);
 prettyPrint();
